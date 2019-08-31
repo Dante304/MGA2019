94058ef99e8a4d13c9dfc1c6695dce828245de17
dante304404 <dante.3.kd@gmail.com>
2019-08-31 14:16:21 +0200

    Dodanie fonta, stworzenie latarki, dodanie intro

---
+++ GameJam2k19/Assets/Scripts/FlashlightScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashlightScript : MonoBehaviour
+{
+    Vector3 mousePosition;
+
+    void Update()
+    {
+        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        this.transform.position = new Vector2(mousePosition.x, mousePosition.y);
+    }
+}
