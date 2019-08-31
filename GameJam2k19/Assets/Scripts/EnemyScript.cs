61ec4b8cea8403660a9e0cad7e22e7da98952312
dante304404 <dante.3.kd@gmail.com>
2019-08-31 16:10:25 +0200

    Dodanie prpzeciwnika, zlozenie dema level1

---
+++ GameJam2k19/Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+    public float speed;
+    public float distanceToChangeSide;
+    public Transform startPosition;
+    public Transform stopPosition;
+    public bool toStop;
+
+    private void Update()
+    {
+        if (toStop)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, stopPosition.position, speed);
+            if (transform.position.x >= stopPosition.position.x )
+            {
+                toStop = false;
+            }
+            
+        }
+        else
+        {
+            transform.position = Vector2.MoveTowards(transform.position, startPosition.position, speed);
+            if (transform.position.x <= startPosition.position.x)
+            {
+                toStop = true;
+            }
+        }
+    }
+}
