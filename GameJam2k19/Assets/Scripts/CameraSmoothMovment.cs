eebe514e8d119b1688b2c33ad901812e117b515a
Bartosz Jasak <b.jasak@student.po.edu.pl>
2019-08-31 17:58:43 +0200

    Dodanie skryptu odpowiedzialnego za Poruszanie płynne kamera oraz skryptu odpowidzialnego za Grawitacje

---
+++ GameJam2k19/Assets/Scripts/CameraSmoothMovment.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSmoothMovment : MonoBehaviour
+{
+    private Vector2 velocity;
+    public float smoothTimeX;
+    public float smoothTimeY;
+
+    public GameObject player;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+
+    }
+
+    private void FixedUpdate()
+    {
+        float posX = Mathf.SmoothDamp(transform.position.x, player.transform.position.x, ref velocity.x, smoothTimeX);
+        
+        float posY = Mathf.SmoothDamp(transform.position.y+1, player.transform.position.y+1, ref velocity.y, smoothTimeY);
+        
+        transform.position = new Vector3(posX, posY, transform.position.z);
+
+
+    }
+
+
+}
---
+++ GameJam2k19/Assets/Scripts/GravityController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityController : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+
+    private void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            rb2d.gravityScale *= -1;
+        }
+    }
+
+}
