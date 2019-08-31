e4e60c2bf31faa10dd18a66caed3e5dcf9a1e590
dante304404 <dante.3.kd@gmail.com>
2019-08-31 19:54:31 +0200

    Drzewo i babcia

---
+++ GameJam2k19/Assets/Scripts/ShowTextScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowTextScript : MonoBehaviour
+{
+    public GameObject textBox;
+    public string text;
+    private bool voicePlay;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+       
+        if (collision.tag == "Player")
+        {
+            voicePlay = true;
+            textBox.SetActive(true);
+            textBox.GetComponentInChildren<Text>().text = text;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        textBox.SetActive(false);
+    }
+}
