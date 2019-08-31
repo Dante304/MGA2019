8c052095a3c001a037fd458936eef8a0ac8e40ef
dante304404 <dante.3.kd@gmail.com>
2019-08-31 17:57:02 +0200

    Dodanie checkpointow, obrazen od wroga

---
+++ GameJam2k19/Assets/Scripts/PlayerCheckpoint.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCheckpoint : MonoBehaviour
+{
+    public List<Collider2D> listOfCheckpoints;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!listOfCheckpoints.Contains(collision) && collision.tag == "Checkpoint")
+        {
+            listOfCheckpoints.Add(collision);
+        }
+
+        if (collision.tag == "Enemy")
+        {
+            var lastCheckpoint = listOfCheckpoints.Count - 1;
+            var checkpointTransform = listOfCheckpoints[lastCheckpoint].transform.position;
+            this.transform.position = new Vector2(checkpointTransform.x, checkpointTransform.y);
+        }
+    }
+
+
+}
