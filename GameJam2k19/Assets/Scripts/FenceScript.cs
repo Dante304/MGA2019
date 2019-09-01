c2eef36d394b9a0370ad5ee9611bf5f7656d3cd6
dante304404 <dante.3.kd@gmail.com>
2019-09-01 02:39:48 +0200

    Level1 zrobiony

---
+++ GameJam2k19/Assets/Scripts/FenceScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FenceScript : MonoBehaviour
+{
+    public GameObject fence;
+
+    private void OnMouseEnter()
+    {
+        fence.GetComponent<BoxCollider2D>().isTrigger = true;
+    }
+
+    private void OnMouseExit()
+    {
+        fence.GetComponent<BoxCollider2D>().isTrigger = false ;
+    }
+
+}
