049503a6f97972c2ebb46784fe3bd5518ca8a778
dante304404 <dante.3.kd@gmail.com>
2019-09-01 08:18:40 +0200

    Level 1 zmiany

--- GameJam2k19/Assets/Scripts/ClockRiddleScript.cs
+++ GameJam2k19/Assets/Scripts/ClockRiddleScript.cs
@@ -8,6 +8,7 @@ public class ClockRiddleScript : MonoBehaviour
     public static int clock2 = 6;
     public static int clock3 = 6;
     public static int clock4 = 6;
+    public GameObject end;
     private static bool doorOpened = false;
     void Update()
     {
@@ -15,11 +16,13 @@ public class ClockRiddleScript : MonoBehaviour
     }
     public void ClockSolution()
     {
-        if (ClockRiddleScript.clock1 % 12 == 0 && ClockRiddleScript.clock2 % 12 == 0 && ClockRiddleScript.clock3 % 12 == 0)
+        if (ClockRiddleScript.clock1 % 12 == 0 && ClockRiddleScript.clock2 % 12 == 0 && ClockRiddleScript.clock3 % 12 == 0 && ClockRiddleScript.clock4 % 12 == 0)
         {
             //zaprogramować otwarcie drzwi
             doorOpened = true;
             Debug.Log("Drzwi otwarte");
+            GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>().schoolEnd = true;
+            end.SetActive(true);
         }
     }
 }
